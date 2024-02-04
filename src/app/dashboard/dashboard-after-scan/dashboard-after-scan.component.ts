bef0696f3cfe54c90f1987c44d579043afd3c27b
vikrant29k <vikrant.mobicloud@gmail.com>
2024-02-04 22:54:46 +0530

    added selected image

--- src/app/dashboard/dashboard-after-scan/dashboard-after-scan.component.ts
+++ src/app/dashboard/dashboard-after-scan/dashboard-after-scan.component.ts
@@ -11,18 +11,29 @@ vinNumber:any
   constructor(private activeRoute:ActivatedRoute,private http:HttpClient){
 
   }
-  isButtonDivVisible = true;
-  back(){
-    this.isButtonDivVisible=true
-  }
+  isButtonDivVisible:any = true;
+
   toggleDivs() {
     this.isButtonDivVisible = !this.isButtonDivVisible;
   }
 ngOnInit(): void {
     this.vinNumber =  this.activeRoute.snapshot.params['vinNumber']
-    this.http.get(`http://192.168.29.78:8000/api/scan/${this.vinNumber}`).subscribe(res=>{
-      console.log("response", res)
-    })
+    this.wheelsData.forEach(wheel => {
+      wheel.checked = false;
+    });  this.logo.forEach(logo => {
+      logo.checked = false;
+    });
+    console.log(this.wheelsData)
+    // this.http.get(`http://192.168.29.78:8000/api/scan/${this.vinNumber}`).subscribe(
+    //   (res) => {
+    //     console.log("response", res);
+    //     // this.getImages()
+    //   },
+    //   (error) => {
+    //     console.error("Error occurred:", error);
+    //   }
+    // );
+
 }
 
 activeDiv: number = 1;
@@ -33,6 +44,74 @@ next(){
   this.activeDiv++
   if(this.activeDiv>4){
     this.activeDiv=1
+  }
+}
+baseUrl='http://192.168.29.78:8000/api'
+wheelsData:any[]=[
+  {
+    "id": 1,
+    "title": "Wheel",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1VMQzlDsK2czEifi1Ixz_OKI46R1dH_NFxcTkGFYjPA&s"
+  },
+  {
+    "id": 2,
+    "title": "Wheel",
+    "image": "https://cdn1.vectorstock.com/i/1000x1000/43/85/car-wheel-vector-34385.jpg"
+  },
+  {
+    "id": 3,
+    "title": "Wheel",
+    "image": "https://static6.depositphotos.com/1096434/587/v/450/depositphotos_5874149-stock-illustration-car-wheel.jpg"
+  }
+];
+logo:any[]=[
+  {
+    "id": 1,
+    "title": "Wheel",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1VMQzlDsK2czEifi1Ixz_OKI46R1dH_NFxcTkGFYjPA&s"
+  },
+  {
+    "id": 2,
+    "title": "Wheel",
+    "image": "https://cdn1.vectorstock.com/i/1000x1000/43/85/car-wheel-vector-34385.jpg"
+  },
+  {
+    "id": 3,
+    "title": "Wheel",
+    "image": "https://static6.depositphotos.com/1096434/587/v/450/depositphotos_5874149-stock-illustration-car-wheel.jpg"
+  }
+];
+// getImages(){
+//   this.http.get('http://192.168.29.78:8000/api/wheelimages/').subscribe((res:any)=>{
+//     this.wheelsData=res.WheelImages;
+
+//   })
+// }
+selectedImages:any[]=[]
+handleCheckboxChange(index: number): void {
+  const selectedWheel = this.wheelsData[index];
+
+  // Toggle the checked state
+  selectedWheel.checked = !selectedWheel.checked;
+
+  // Add or remove from the selectedImages array based on the checked state
+  if (selectedWheel.checked) {
+    this.selectedImages.push(selectedWheel.image);
+  } else {
+    const indexToRemove = this.selectedImages.indexOf(selectedWheel.image);
+    if (indexToRemove !== -1) {
+      this.selectedImages.splice(indexToRemove, 1);
+    }
+  }
+}
+isSubmitted: boolean = false;
+submit(): void {
+  this.isSubmitted = true;
+}
+back(){
+  this.isButtonDivVisible=true
+  if(this.isSubmitted==true){
+    this.isSubmitted = false;
   }
 }
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -42,4 +42,5 @@ getValue(value:any){
 
 }
 
+
 }
